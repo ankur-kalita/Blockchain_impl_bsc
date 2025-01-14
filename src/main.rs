1843a58bf4bec1e352d52a2637853d312e4b071f
AnkuR0124 <ankur.23bcs10185@ms.sst.scaler.com>
2025-01-15 02:58:27 +0530

    basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use core::hash;
-use std::fmt::format;
-use sha256::digest;
 use chrono::{DateTime, Utc};
+use core::hash;
+use sha256::digest;
+use std::fmt::format;
 
 #[derive(Debug, Clone)]
 
@@ -36,6 +36,49 @@ impl Blockchain {
         };
         self.blocks.push(genesis_block);
     }
+    fn try_add_block(&mut self, block: Block) {
+        match self.blocks.last() {
+            None => {
+                println!("The blockchain does not have atleast one block!");
+                return;
+            }
+            Some(latest_block) => {
+                if self.is_block_valid(&block, latest_block) {
+                    self.blocks.push(block);
+                    println!("Block has been successfully added");
+                } else {
+                    println!("Could not add block, invalid!");
+                }
+            }
+        }
+    }
+    fn is_block_valid(&self, new_block: &Block, latest_block: &Block) -> bool {
+        if new_block.previous_hash != latest_block.hash {
+            println!("Block with id {} has wrong previous hash", new_block.id);
+            return false;
+        } else if !new_block.hash.starts_with("0000") {
+            println!("Block with id: {} has invalid hash", new_block.id);
+            return false;
+        } else if new_block.id != latest_block.id + 1 {
+            println!(
+                "Block with id {} is not the next block after the latest block with id: {}",
+                new_block.id, latest_block.id
+            );
+            return false;
+        } else if digest(format!(
+            "{}{}{}{}{}",
+            new_block.id,
+            &new_block.previous_hash,
+            &new_block.data,
+            new_block.timestamp,
+            new_block.nonce
+        )) != new_block.hash
+        {
+            println!("Block with id {} is invalid hash", new_block.id);
+            return false;
+        }
+        true
+    }
 }
 
 impl Block {
@@ -44,25 +87,26 @@ impl Block {
         let now_timestamp: i64 = now.timestamp();
         let (nonce, hash) = Block::mine_block(id, now_timestamp, &previous_hash, &data);
         Self {
-            id, 
+            id,
             hash,
             timestamp: now.timestamp(),
             previous_hash,
             data,
-            nonce
+            nonce,
         }
     }
     fn mine_block(id: u64, timestamp: i64, previous_hash: &str, data: &str) -> (u64, String) {
         println!("mining block...");
-        let mut nonce= 1;
+        let mut nonce = 1;
         loop {
-            let block_string: String = format!("{}{}{}{}{}", id, previous_hash, data, timestamp, nonce);
+            let block_string: String =
+                format!("{}{}{}{}{}", id, previous_hash, data, timestamp, nonce);
             let hash: String = digest(block_string);
             if hash.starts_with("0000") {
                 println!("mined! nonce: {}, hash: {}", nonce, hash);
-                return (nonce, hash)
+                return (nonce, hash);
             }
-            nonce += 1;  
+            nonce += 1;
         }
     }
 }
@@ -71,5 +115,6 @@ fn main() {
     new_BC.starting_block();
 
     println!("{:?}", new_BC);
-    let new_block: Block = Block::new( 2,  new_BC.blocks[0].hash.to_owned(),"Ankur".to_string());
+    let new_block: Block = Block::new(2, new_BC.blocks[0].hash.to_owned(), "Ankur".to_string());
+    new_BC.try_add_block(new_block);
 }
